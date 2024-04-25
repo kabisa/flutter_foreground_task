98556116603e56c38de03c510266212287ce9705
Max Bodelier <m.a.j.bodelier@gmail.com>
2024-04-25 11:20:15 +0200

    refactor bug in intent receiver

--- android/src/main/kotlin/com/pravera/flutter_foreground_task/service/IntentReceiver.kt
+++ android/src/main/kotlin/com/pravera/flutter_foreground_task/service/IntentReceiver.kt
@@ -9,23 +9,23 @@ import com.pravera.flutter_foreground_task.models.ForegroundServiceStatus
 import com.pravera.flutter_foreground_task.models.ForegroundTaskOptions
 
 /**
- * The receiver that receives the BOOT_COMPLETED event.
+ * The receiver that receives the BOOT_COMPLETED and MY_PACKAGE_REPLACED event.
  *
  * @author Dev-hwang
  * @version 1.0
  */
 class IntentReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (context != null && (intent?.action == Intent.ACTION_BOOT_COMPLETED || intent?.action == Intent.ACTION_MY_PACKAGE_REPLACED)) {
+        val options = ForegroundTaskOptions.getData(context)
 
-            val options = ForegroundTaskOptions.getData(context)
+        if(context == null) return
+        // Check whether to start the service at boot time.
+        if(intent?.action == Intent.ACTION_BOOT_COMPLETED && !options.autoRunOnBoot) return
+        //Check whether to start the service on my package replaced time.
+        if(intent?.action == Intent.ACTION_MY_PACKAGE_REPLACED && !options.autoRunOnMyPackageReplaced) return
 
-            // Check whether to start the service at boot time.
-            if (!options.autoRunOnBoot) return
 
-            //Check whether to start the service on my package replaced time.
-            if (!options.autoRunOnMyPackageReplaced) return
-
+        if(intent?.action == Intent.ACTION_BOOT_COMPLETED || intent?.action == Intent.ACTION_MY_PACKAGE_REPLACED)) {
             // Create an intent for calling the service and store the action to be executed
             val nIntent = Intent(context, ForegroundService::class.java)
             ForegroundServiceStatus.putData(context, ForegroundServiceAction.REBOOT)
